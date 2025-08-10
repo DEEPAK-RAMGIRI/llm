b06c12976d6b1e8408b56aaa67fc4f25c35a0c0a
RAMGIRI  DEEPAK KUMAR <142658497+DEEPAK-RAMGIRI@users.noreply.github.com>
2025-08-10 21:08:46 +0530

    Update main.py

--- MyBot/main.py
+++ MyBot/main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from llm import create_vector_db,Question_and_answer
 
-btn = st.button("Create Knowledgebase")
-if btn:
-    create_vector_db()
+# btn = st.button("Create Knowledgebase")
+# if btn:
+#     create_vector_db()
 
 question = st.text_input("Question: ")
 
